626fc15a081d6f5d5eb2cb2231566345414f81a3
LUQQ0 <88996242+LUQQ0@users.noreply.github.com>
2026-06-26 04:17:21 +0800

    Add files via upload

---
+++ 2026_06_19-Task_4/task_4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    string data;
+    Node *next; // pointer variable, stores address
+    Node *prev;
+};
+
+int main()
+{
+
+    Node *node1 = new Node();
+    Node *node2 = new Node();
+    Node *node3 = new Node();
+    Node *node4 = new Node();
+
+    node1->data = "Aimar";
+    node1->next = node2;
+    node1->prev = NULL;
+
+    node2->data = "Ahmad";
+    node2->next = node3;
+    node2->prev = node1;
+
+    node3->data = "anjana";
+    node3->next = node4;
+    node3->prev = node2;
+
+    node4->data = "Jessy";
+    node4->next = NULL;
+    node4->prev = node3;
+
+    // Traverse and print
+    Node *temp = node1;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
